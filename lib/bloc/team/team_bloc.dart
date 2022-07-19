e0349c19cd389572a42908fa053ff7add0589a2c
Ta Nhat <minhnhat3105@gmail.com>
2022-07-19 08:30:38 +0700

    finish skeleton bloc pattern for team, competition, seeds-wallet

---
+++ lib/bloc/team/team_bloc.dart
@@ -0,0 +1,22 @@
+import 'package:unicec_mobi/bloc/team/team_event.dart';
+import 'package:unicec_mobi/bloc/team/team_state.dart';
+import 'package:unicec_mobi/models/entities/team/team_model.dart';
+import 'package:unicec_mobi/models/enums/team_status.dart';
+import 'package:unicec_mobi/services/i_services.dart';
+import 'package:unicec_mobi/utils/base_bloc.dart';
+
+class TeamBloc extends BaseBloc<TeamEvent, TeamState> {
+  final ITeamService service;
+
+  TeamBloc({required this.service})
+      : super(TeamState(
+            team: TeamModel(
+                id: 0,
+                competitionId: 0,
+                name: '',
+                description: '',
+                invitedCode: '',
+                status: TeamStatus.Available))){
+                  
+                }
+}
