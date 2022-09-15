ee4bd2aeb40f37dfc91c3ffc231d1f1c8e96e0af
NguyenTienAnh <anhnguyentien3006@gmail.com>
2022-09-15 13:48:07 +0700

    fix detail team in match

--- lib/bloc/view_detail_match/view_detail_match_state.dart
+++ lib/bloc/view_detail_match/view_detail_match_state.dart
@@ -3,14 +3,19 @@ import 'package:unicec_mobi/models/entities/match/match_model.dart';
 
 import '../../models/entities/match/teams_in_match_model.dart';
 
-class ViewDetailMatchState extends Equatable{
+class ViewDetailMatchState extends Equatable {
   MatchModel match;
   List<TeamsInMatchModel> teamsInMatch;
 
   ViewDetailMatchState({required this.match, required this.teamsInMatch});
 
+  ViewDetailMatchState copyWith(
+      {required MatchModel match,
+      required List<TeamsInMatchModel> teamsInMatch}) {
+    return ViewDetailMatchState(match: match, teamsInMatch: teamsInMatch);
+  }
+
   @override
   // TODO: implement props
-  List<Object?> get props => [match];
-
+  List<Object?> get props => [match, teamsInMatch];
 }
